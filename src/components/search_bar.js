359f2e23f5773e1384d6c437a147301dee35fc4f
Freud Romero <abnertorres58@gmail.com>
2018-05-02 10:41:40 -0400

    Understanding State and controling components

---
+++ src/components/search_bar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+
+//Functional component
+// const SearchBar = () => {
+//   return <input />
+// }
+
+class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {term: 'Starting value' }
+  }
+
+  render () {
+    return (
+      <div>
+        <input
+          value = {this.state.term}
+          onChange={(event) => this.setState({ term: event.target.value})} />
+      </div>
+
+    );
+  }
+
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyA0qSHkaTC6hucOsDrrWM_F2yPw7LKAzEo'
+// This is the way to create a new component that produces some HTML
 const App = () => {
-  return <div>Hi Fred!</div>
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  )
 }
 
+
+//Takes the generated component and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container') )
