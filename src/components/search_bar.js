dce80bee21a5c5de334ef920f2b072d0c865e61b
Freud Romero <abnertorres58@gmail.com>
2018-05-02 20:48:52 -0400

    handeling props between components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,7 +10,7 @@ class SearchBar extends Component {
   constructor(props){
     super(props);
 
-    this.state = {term: 'Starting value' }
+    this.state = {term: '' }
   }
 
   render () {
--- src/index.js
+++ src/index.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import SearchBar from './components/search_bar'
+const API_KEY = 'AIzaSyA0qSHkaTC6hucOsDrrWM_F2yPw7LKAzEo';
 
-const API_KEY = 'AIzaSyA0qSHkaTC6hucOsDrrWM_F2yPw7LKAzEo'
-// This is the way to create a new component that produces some HTML
-const App = () => {
-  return (
-  <div>
-    <SearchBar />
-  </div>
-  )
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        {/*Passing props in React*/}
+        <VideoList
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 
 
-//Takes the generated component and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container') )
